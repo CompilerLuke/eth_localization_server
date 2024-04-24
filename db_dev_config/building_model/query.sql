f7329e2dc5388d866efad441ca1211fe0249397b
Lucas Goetz <goetzl@student.ethz.ch>
2024-04-24 16:24:59 +0200

    Testing passive localization

--- db_dev_config/building_model/query.sql
+++ db_dev_config/building_model/query.sql
@@ -4,7 +4,7 @@
 
 -- SELECT * FROM BuildingModel.Location where building = 45 and label LIKE '2%'
 
--- SELECT * FROM BuildingModel.Buildings
+SELECT * FROM BuildingModel.[Location];
 
 --INSERT INTO BuildingModel.Buildings ([username])
 --VALUES ('hello')
@@ -12,4 +12,4 @@
 --INSERT INTO BuildingScan.FileUpload(username, building, filename, fileType)
 --VALUES ('hello', 17, 'hello', '_')
 
-SELECT * FROM BuildingScan.FileUpload;
+--SELECT * FROM BuildingScan.FileUpload;
